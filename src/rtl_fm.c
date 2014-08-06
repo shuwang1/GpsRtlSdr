3911cc5fa1f4d103748b7d905f368c839838b043
Kyle Keen <keenerd@gmail.com>
2014-08-05 23:31:52 -0400

    rtl_fm: software agc

--- src/rtl_fm.c
+++ src/rtl_fm.c
@@ -30,18 +30,14 @@
  *
  * todo:
  *       sanity checks
- *       scale squelch to other input parameters
- *       test all the demodulations
  *       pad output on hop
  *       frequency ranges could be stored better
- *       scaled AM demod amplification
  *       auto-hop after time limit
  *       peak detector to tune onto stronger signals
  *       fifo for active hop frequency
  *       clips
  *       noise squelch
  *       merge stereo patch
- *       merge soft agc patch
  *       merge udp patch
  *       testmode to detect overruns
  *       watchdog to reset bad dongle
@@ -125,6 +121,17 @@ struct dongle_state
 	struct demod_state *demod_target;
 };
 
+struct agc_state
+{
+	int32_t gain_num;
+	int32_t gain_den;
+	int32_t gain_max;
+	int     peak_target;
+	int     attack_step;
+	int     decay_step;
+	int     error;
+};
+
 struct demod_state
 {
 	int      exit_flag;
@@ -158,6 +165,8 @@ struct demod_state
 	int32_t  angle_b;  /* constant, sin(rotate_angle) * ONE_INT */
 	int32_t  angle_sin;  /* iterative */
 	int32_t  angle_cos;
+	int      agc_enable;
+	struct   agc_state *agc;
 	void     (*mode_demod)(struct demod_state*);
 	pthread_rwlock_t rw;
 	pthread_cond_t ready;
@@ -222,8 +231,9 @@ void usage(void)
 		"\t[-E enable_option (default: none)]\n"
 		"\t    use multiple -E to enable multiple options\n"
 		"\t    edge:   enable lower edge tuning\n"
-		"\t    dc:     enable dc blocking filter\n"
+		"\t    no-dc:  disable dc blocking filter\n"
 		"\t    deemp:  enable de-emphasis filter\n"
+		"\t    swagc:  enable software agc (only for AM modes)\n"
 		"\t    direct: enable direct sampling\n"
 		"\t    no-mod: enable no-mod direct sampling\n"
 		"\t    offset: enable offset tuning\n"
@@ -622,7 +632,7 @@ void fm_demod(struct demod_state *fm)
 void am_demod(struct demod_state *fm)
 // todo, fix this extreme laziness
 {
-	int i, pcm;
+	int32_t i, pcm;
 	int16_t *lp = fm->lowpassed;
 	for (i = 0; i < fm->lp_len; i += 2) {
 		// hypot uses floats but won't overflow
@@ -632,7 +642,7 @@ void am_demod(struct demod_state *fm)
 		lp[i/2] = (int16_t)sqrt(pcm) * fm->output_scale;
 	}
 	fm->lp_len = fm->lp_len / 2;
-	// lowpass? (3khz)  highpass?  (dc)
+	// lowpass? (3khz)
 }
 
 void usb_demod(struct demod_state *fm)
@@ -752,9 +762,45 @@ int squelch_to_rms(int db, struct dongle_state *dongle, struct demod_state *demo
 	return (int)linear + 1;
 }
 
+void software_agc(struct demod_state *d)
+{
+	int i = 0;
+	int peaked = 0;
+	int32_t output;
+	struct agc_state *agc = d->agc;
+	int16_t *lp = d->lowpassed;
+
+	for (i=0; i < d->lp_len; i++) {
+		output = (int32_t)lp[i] * agc->gain_num + agc->error;
+		agc->error = output % agc->gain_den;
+		output /= agc->gain_den;
+
+		if (!peaked && abs(output) > agc->peak_target) {
+			peaked = 1;}
+		if (peaked) {
+			agc->gain_num += agc->attack_step;
+		} else {
+			agc->gain_num += agc->decay_step;
+		}
+
+		if (agc->gain_num < agc->gain_den) {
+			agc->gain_num = agc->gain_den;}
+		if (agc->gain_num > agc->gain_max) {
+			agc->gain_num = agc->gain_max;}
+
+		if (output >= (1<<15)) {
+			output = (1<<15) - 1;}
+		if (output < -(1<<15)) {
+			output = -(1<<15) + 1;}
+
+		lp[i] = (int16_t)output;
+	}
+}
+
 void full_demod(struct demod_state *d)
 {
 	int i, ds_p;
+	int do_squelch = 0;
 	int sr = 0;
 	ds_p = d->downsample_passes;
 	if (ds_p) {
@@ -777,25 +823,32 @@ void full_demod(struct demod_state *d)
 	if (d->squelch_level) {
 		sr = rms(d->lowpassed, d->lp_len, 1);
 		if (sr < d->squelch_level) {
-			d->squelch_hits++;
-			for (i=0; i<d->lp_len; i++) {
-				d->lowpassed[i] = 0;
-			}
-		} else {
-			d->squelch_hits = 0;}
+			do_squelch = 1;}
+	}
+	if (do_squelch) {
+		d->squelch_hits++;
+		for (i=0; i<d->lp_len; i++) {
+			d->lowpassed[i] = 0;
+		}
+	} else {
+		d->squelch_hits = 0;
+	}
+	if (d->squelch_level && d->squelch_hits > d->conseq_squelch) {
+		d->agc->gain_num = d->agc->gain_den;
 	}
 	d->mode_demod(d);  /* lowpassed -> lowpassed */
 	if (d->mode_demod == &raw_demod) {
-		return;
-	}
+		return;}
+	if (d->dc_block) {
+		dc_block_filter(d);}
+	if (d->agc_enable) {
+		software_agc(d);}
 	/* todo, fm noise squelch */
 	// use nicer filter here too?
 	if (d->post_downsample > 1) {
 		d->lp_len = low_pass_simple(d->lowpassed, d->lp_len, d->post_downsample);}
 	if (d->deemph) {
 		deemph_filter(d);}
-	if (d->dc_block) {
-		dc_block_filter(d);}
 	if (d->rate_out2 > 0) {
 		low_pass_real(d);
 		//arbitrary_resample(d->lowpassed, d->lowpassed, d->lp_len, d->lp_len * d->rate_out2 / d->rate_out);
@@ -1080,13 +1133,14 @@ void demod_init(struct demod_state *s)
 	s->post_downsample = 1;  // once this works, default = 4
 	s->custom_atan = 0;
 	s->deemph = 0;
+	s->agc_enable = 0;
 	s->rate_out2 = -1;  // flag for disabled
 	s->mode_demod = &fm_demod;
 	s->pre_j = s->pre_r = s->now_r = s->now_j = 0;
 	s->prev_lpr_index = 0;
 	s->deemph_a = 0;
 	s->now_lpr = 0;
-	s->dc_block = 0;
+	s->dc_block = 1;
 	s->dc_avg = 0;
 	pthread_rwlock_init(&s->rw, NULL);
 	pthread_cond_init(&s->ready, NULL);
@@ -1153,6 +1207,23 @@ void sanity_checks(void)
 
 }
 
+int agc_init(struct demod_state *s)
+{
+	int i;
+	struct agc_state *agc;
+
+	agc = malloc(sizeof(struct agc_state));
+	s->agc = agc;
+
+	agc->gain_den = 1<<15;
+	agc->gain_num = agc->gain_den;
+	agc->peak_target = 1<<14;
+	agc->gain_max = 256 * agc->gain_den;
+	agc->decay_step = 1;
+	agc->attack_step = -2;
+	return 0;
+}
+
 int generate_header(struct demod_state *d, struct output_state *o)
 {
 	int i, s_rate, b_rate;
@@ -1197,6 +1268,7 @@ int main(int argc, char **argv)
 	int custom_ppm = 0;
 	dongle_init(&dongle);
 	demod_init(&demod);
+	agc_init(&demod);
 	output_init(&output);
 	controller_init(&controller);
 
@@ -1251,10 +1323,12 @@ int main(int argc, char **argv)
 		case 'E':
 			if (strcmp("edge",  optarg) == 0) {
 				controller.edge = 1;}
-			if (strcmp("dc", optarg) == 0) {
-				demod.dc_block = 1;}
+			if (strcmp("no-dc", optarg) == 0) {
+				demod.dc_block = 0;}
 			if (strcmp("deemp",  optarg) == 0) {
 				demod.deemph = 1;}
+			if (strcmp("swagc",  optarg) == 0) {
+				demod.agc_enable = 1;}
 			if (strcmp("direct",  optarg) == 0) {
 				dongle.direct_sampling = 1;}
 			if (strcmp("no-mod",  optarg) == 0) {
